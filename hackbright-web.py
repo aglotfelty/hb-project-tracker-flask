a4beaf0c05c0ab6e4f6dc005eaaae6c4ca51a2bd
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-14 17:21:35 -0700

    Created form to add a student to database and updated /student route to display project grades

---
+++ hackbright-web.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get("github", "jhacks")
+
+    first, last, github = hackbright.get_student_by_github(github)
+    grades = hackbright.get_grades_by_github(github)
+
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github,
+                           grades=grades)
+
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+
+@app.route("/student-adder")
+def add_a_student():
+    """Shows a form for adding a new student to database."""
+
+    return render_template("student_add.html")
+
+
+@app.route("/student-added", methods=['POST'])
+def student_added():
+    """Add a student."""
+
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    github = request.form.get("github")
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("student_add_success.html", first_name=first_name,
+                                                       last_name=last_name,
+                                                       github=github)
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
